8dc632d3b5f1b5de5758f838f61ae483a98c0010
DevSaadia <saadia.shahid@torontomu.ca>
2024-10-17 20:58:33 -0400

    add signin page + next ui

---
+++ frontend/app/signin/page.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import { Input } from "@nextui-org/input";
+import { Button, ButtonGroup } from "@nextui-org/button";
+
+const signin = () => {
+  return (
+    <section className="bg-[#070B27] flex justify-center items-center w-screen h-screen">
+      <div className=" flex flex-col h-full w-full xl:w-1/2 justify-center items-center">
+        <Image src="/logo.png" alt="logo" width={100} height={100} />
+        <div className="bg-white gap-4 h-fit p-10 w-1/2 flex flex-col justify-center items-center">
+          <div className="flex justify-start w-full">
+            <Button radius="none" className="bg-[#070b27] text-white">
+              STUDENT
+            </Button>
+            <Button radius="none" variant="bordered">
+              INSTRUCTOR
+            </Button>
+          </div>
+          <div className="bg-[#e3e3e3] w-full gap-2 p-6 h-fit flex flex-col items-center justify-center">
+            <Input
+              type="email"
+              label="Gould Email"
+              variant="bordered"
+              className="bg-white border-[#aaaaaa] "
+              radius="none"
+              isRequired
+            />
+            <Input
+              type="text"
+              label="Gould Password"
+              variant="bordered"
+              className="bg-white "
+              radius="none"
+              isRequired
+            />
+          </div>
+          <div className="w-full justify-start flex">
+            <Button className="bg-[#ffe315] font-semibold px-8">Sign in</Button>
+          </div>
+          <Button className="text-blue-400 bg-white font-semibold">
+            Cancel Sign In
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default signin;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
+const {nextui} = require('@nextui-org/theme');
 /** @type {import('tailwindcss').Config} */
+import { nextui } from "@nextui-org/react";
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,5 +17,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
